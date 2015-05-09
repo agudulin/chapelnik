f9bde3e416bf6720a66c558364aed50ae2c25883
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-09 16:15:41 +0300

    add twitter api secret keys to the config file

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,11 @@
 //
 
 module.exports = {
-  db: "mongodb://chapelnik_username:chapelnik_password@ds037387.mongolab.com:37387/chapelnik"
+  db: "mongodb://chapelnik_username:chapelnik_password@ds037387.mongolab.com:37387/chapelnik",
+  twitter: {
+    consumer_key: "rNDo8J7h3ZNBt3qH31gyTGUDw",
+    consumer_secret: "3AEJKRvg7OPtg4OiByrRCB9O9fQ6vCxRaxuxzbmeMSanmSp1T7",
+    access_token_key: "125112832-lMCsuPBqHlIhSCWGTzjyI6O2RTFRYj6p9d7gKNaQ",
+    access_token_secret: "qCLQOUBaG0ys4nJjGT5PsFI21vLOYLS9OX3ioaShYl9ec"
+  }
 };
