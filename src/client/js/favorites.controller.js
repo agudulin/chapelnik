99ab9dca807da4c20f4454115b164e3e84982e64
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-10 19:48:34 +0300

    add favorites template with a dumb controller

---
+++ src/client/js/favorites.controller.js
@@ -0,0 +1,13 @@
+(function() {
+
+  angular
+    .module("Chapelnik")
+    .controller("FavoritesController", FavoritesController);
+
+  // FavoritesController.$inject = [];
+
+  function FavoritesController() {
+    var vm = this;
+  }
+
+})();
