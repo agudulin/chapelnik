e2027adda00868ac8ec95a1a74b2833097240fef
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-11 01:37:25 +0300

    read favorites from the db using long polling

--- src/client/js/favorites.controller.js
+++ src/client/js/favorites.controller.js
@@ -4,10 +4,19 @@
     .module("Chapelnik")
     .controller("FavoritesController", FavoritesController);
 
-  // FavoritesController.$inject = [];
+  FavoritesController.$inject = ["$timeout", "TweetModel"];
 
-  function FavoritesController() {
+  function FavoritesController($timeout, TweetModel) {
     var vm = this;
+
+    startPolling();
+
+    function startPolling() {
+      TweetModel.query(function(tweetList){
+        vm.tweetList = tweetList;
+        $timeout(startPolling, 1000);
+      });
+    }
   }
 
 })();
