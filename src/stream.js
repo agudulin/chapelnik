d64412d60ff3107e7edd78a388da6d40cd8b0171
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-10 03:01:14 +0300

    parse twitter stream using sockets

--- src/stream.js
+++ src/stream.js
@@ -1,10 +1,49 @@
 var socketIO = require("socket.io");
+var Twitter = require("twitter");
+
+var config = require("./config");
 
 function Stream(server) {
   var io = socketIO.listen(server);
+  var twitterClient = new Twitter(config.twitter);
+
+  var currentStream = null;
 
   io.sockets.on("connection", function(socket) {
-    socket.emit("message", {"message": "hello world"});
+
+    socket.on("startStreaming", function(data) {
+      if (!data || !data.query) {
+        socket.emit("err", { error: "Empty data query parameter" });
+        return;
+      }
+
+      twitterClient.stream("statuses/filter", { track: data.query }, function(stream) {
+        currentStream = stream;
+
+        stream.on("data", function(tweets) {
+          socket.emit("newTweets", tweets);
+        });
+
+        stream.on("error", function(error) {
+          socket.emit("err", { error: error });
+        });
+
+        stream.on("end", function() {
+          socket.emit("stop");
+        });
+
+        setTimeout(function(){
+          stream.destroy();
+        }, 20 * 1000);
+      });
+    });
+
+    socket.on("stopStreaming", function() {
+      if (currentStream) {
+        currentStream.destroy();
+        currentStream = null;
+      }
+    });
   });
 }
 
