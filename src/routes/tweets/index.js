d9da2108b66ca1d988f472aa0e3321ca319479e7
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-11 01:30:26 +0300

    routes for adding/reading favorite tweets

--- src/routes/tweets/index.js
+++ src/routes/tweets/index.js
@@ -1,13 +1,33 @@
 var express = require("express");
 var Twitter = require("twitter");
 
+var Tweet = require("../../model/tweet");
+
 var config = require("../../config");
 var twitterClient = new Twitter(config.twitter);
 
 var app = module.exports = express();
 
-app.get("/tweets", function(req, res) {
-  twitterClient.get("search/tweets", {q: "eCommerce CRO"}, function(error, tweets, response) {
-    res.send(tweets);
+app.get("/favorites", getFavoriteTweets);
+app.post("/favorites", addTweetToFavorites);
+
+function getFavoriteTweets(req, res) {
+  Tweet.find(function(err, favoriteTweets) {
+    if (err) {
+      return res.status(500).send({ err: err });
+    }
+    res.send(favoriteTweets);
   });
-});
+}
+
+function addTweetToFavorites(req, res) {
+  var tweet = req.body;
+
+  var tweetModel = new Tweet(tweet);
+  tweetModel.save(function(err, favoriteTweets) {
+    if (err) {
+      return res.status(500).send({ err: err });
+    }
+    res.send(favoriteTweets);
+  });
+}
