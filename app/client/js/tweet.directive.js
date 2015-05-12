cb32cfae4dd0b56823b74f1dd0e2b2d6bd3cf117
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-05-12 14:35:36 +0300

    block tweet ui on adding/removing

--- app/client/js/tweet.directive.js
+++ app/client/js/tweet.directive.js
@@ -27,9 +27,11 @@
     function TweetController(TweetModel) {
       var vm = this;
 
+      vm.isProcessing = false;
       vm.toggleFavorites = toggleFavorites;
 
       function toggleFavorites(tweetItem) {
+        vm.isProcessing = true;
         var tweetModel = new TweetModel();
         tweetModel = $.extend(tweetModel, tweetItem); // add tweet item properties to the model
 
@@ -42,13 +44,15 @@
 
       function addToFavorites(tweetModel) {
         tweetModel.$save(function() {
-          // success
+          vm.isProcessing = false;
         });
       }
 
       function removeFromFavorites(tweetModel) {
         tweetModel.$remove(function() {
-          // success
+          // since we are using long polling to refresh a list of favorite tweets
+          // we need some time to get a new list of tweets from the server when processing is finished
+          // so let's don't change isProcessing value when the tweet is already removed
         });
       }
     }
